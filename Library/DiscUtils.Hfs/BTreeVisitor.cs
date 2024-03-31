b5aea66c92ce1a9dae5f5afdd92ff9f19fbcf1fe
ZuGeTor <14806377+zugetor@users.noreply.github.com>
2024-03-31 14:12:42 +0700

    init HFS Library

---
+++ Library/DiscUtils.Hfs/BTreeVisitor.cs
@@ -0,0 +1,5 @@
+﻿namespace DiscUtils.Hfs
+{
+internal delegate int BTreeVisitor<Key>(Key key, byte[] data)
+where Key : BTreeKey;
+}
---
+++ Library/DiscUtils.Hfs/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("86c5f48d-3240-4765-855d-70e765f73e9a")]
